62ad3cae5f6eec7d6dc8729f06f4c00fcc9a8ed7
olli1 <oliverschack@gmail.com>
2023-03-02 11:53:18 +0100

    added navbar and admin login modal

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var logger = require('morgan');
 var livereload = require("livereload");
 var connectLiveReload = require("connect-livereload");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 const liveReloadServer = livereload.createServer();
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
@@ -20,18 +17,15 @@ var app = express();
 
 app.use(connectLiveReload());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '/pages/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
