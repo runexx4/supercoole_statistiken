cf8e84fbacb92854b8e6b4afe206186a586eae1b
olli1 <oliverschack@gmail.com>
2023-04-28 17:35:50 +0200

    darkmode

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,25 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
-    html: true,
-    content: function () {
-        return document.getElementById("popover-settings").innerHTML;
-    },
-}))
+$(function () {
+    $('[data-bs-toggle="popover"]').popover({
+        html: true,
+        placement:"bottom",
+        sanitize: false,
+        content: function () {
+            return $("#popover-settings").html();
+        },
+    })
+})
+
+$('html').on('mouseup', function (e) {
+    if (!$(e.target).closest('.popover').length && !$(e.target).closest('.settingsBtn').length) {
+        $('[data-bs-toggle="popover"]').popover('hide')
+    }
+});
+
+function toggleDarkmode(){
+    console.log($("#darkLight"))
+    if( $("#darkLight").attr("href") === "stylesheets/lightmode.css" ){
+        $("#darkLight").attr("href", "stylesheets/darkmode.css")
+    }else{
+        $("#darkLight").attr("href", "stylesheets/lightmode.css")
+    }
+}
